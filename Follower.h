97eb7ffdce88b384700e2fef10fe011a4cb59d08
Sartigan <sartigan1@gmail.com>
2016-10-02 04:15:55 -0400

    I have no Idea what I'm Doing
    WTF AM I DOING

---
+++ Follower.h
@@ -0,0 +1,35 @@
+#ifndef FOLLOWER_H
+#define FOLLOWER_H
+#pragma warning (disable:4786)
+//------------------------------------------------------------------------
+//
+//  Name:   Follower.h
+//
+//  Desc:   Definition of a simple vehicle that uses steering behaviors
+//
+//  Author: Mat Buckland 2002 (fup@ai-junkie.com)
+//
+//------------------------------------------------------------------------
+#include "MovingEntity.h"
+#include "2d/Vector2D.h"
+#include "misc/Smoother.h"
+#include "Vehicule.h"
+
+#include <vector>
+#include <list>
+#include <string>
+
+class GameWorld;
+class SteeringBehavior;
+class Vehicule;
+
+
+
+class Follower : public Vehicule
+{
+
+}
+
+
+
+#endif
